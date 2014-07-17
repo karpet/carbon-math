b71eb6a72cec15eadd2485b7f4e14a9a05cd1167
Peter Karman <karpet@peknet.com>
2014-07-17 13:02:13 -0500

    latest

--- churn.pl
+++ churn.pl
@@ -28,32 +28,37 @@ close $fh;
 # (0.392/M-0.2253)LN(x)+(0.001563M-0.07114)y^2+(0.0064-0.003905M)y-0.000727M+0.3773
 
 my %C = (
-    A => 0.645701,
-    B => -0.2397,
-    C => 0.001689,
-    D => -0.070588,
-    E => -0.003976,
-    F => -0.006069,
-    G => -0.0007576,
-    H => 0.3759,
+    A => 0.592,
+    B => -0.236,
+    C => 0.0017,
+    D => -0.11,
+    E => -0.004,
+    F => 0.0729,
+    G => -0.00089,
+    H => 0.339,
+    I => 0.0000049,
+    J => .000005,
 );
-my %MV = (
-    A => 0.001,
-    B => 0.001,
-    C => 0.001,
-    D => 0.001,
-    E => 0.001,
-    F => 0.001,
-    G => 0.001,
-    H => 0.001,
+my %Offset = (
+    A => 0,
+    B => 0,
+    C => 0,
+    D => 0,
+    E => 0,
+    F => 0,
+    G => 0,
+    H => 0,
+    I => 0,
+    J => 0,
 );
 
 sub churn {
     my ( $x, $y, $z, $w ) = @_;
     my $eq
-        = ( $C{A} / $z + $C{B} ) * log($x)
-        + ( $C{C} * $z + $C{D} ) * $y * $y
-        + ( $C{E} * $z + $C{F} ) * $y
+        = ( $C{A} / ($z) + $C{B} ) * log($x)
+        + ( $C{I} * ($z) + $C{J} ) * ( $x**7 )
+        + ( $C{C} * ($z) + $C{D} ) * ( $y**2 )
+        + ( $C{E} * ($z) + $C{F} ) * $y
         + $C{G} * $z
         + $C{H};
     my $err = ( $w - $eq )**2;
@@ -66,30 +71,51 @@ sub churn_loop {
     my ($prev_err_sum) = @_;
 
     # apply $mv against each value in %C
-    my @errs = ();
-    for my $k ( ( 'A' .. 'H' ) ) {
-        my $err_sum = 0;
-        $C{$k} += ( $MV{$k} );
-        for my $row (@Ws) {
-            $err_sum += churn( map { $row->{$_} } qw( x y z w ) );
-        }
-        if ( $err_sum > $prev_err_sum ) {
-            my $err_sum = 0;
-            $C{$k} += -( $MV{$k} * 2 );
-            for my $row (@Ws) {
-                $err_sum += churn( map { $row->{$_} } qw( x y z w ) );
-            }
-
-            # last sanity check
-            if ( $err_sum > $prev_err_sum ) {
-                $C{$k} += ( $MV{$k} );
-                if ( $MV{$k} > 0.00000001 ) {
-                    $MV{$k} = 0.95 * $MV{$k};
+    my @errs    = ();
+    my $err_sum = 0;
+    for my $k ( ( 'A' .. 'J' ) ) {
+        my $pre_move = $C{$k};
+        for my $move ( ( 0 .. 7 ) ) {
+            my $down = 1;
+            while ( $down == 1 ) {
+                $err_sum = 0;
+                my $ck_temp = $C{$k};
+                $C{$k} += ( 10**( $Offset{$k} - $move ) );
+                for my $row (@Ws) {
+                    $err_sum += churn( map { $row->{$_} } qw( x y z w ) );
                 }
+                if ( $err_sum < $prev_err_sum ) {
+                    $prev_err_sum = $err_sum;
+                }
+                else {
+                    $down = 0;
+                    $C{$k} = $ck_temp;
+                }
+
+                #printf ( "$C{$k}  $err_sum\n");
+            }
+            $down = 1;
+            while ( $down == 1 ) {
+                $err_sum = 0;
+                my $ck_temp = $C{$k};
+                $C{$k} -= ( 10**( $Offset{$k} - $move ) );
+                for my $row (@Ws) {
+                    $err_sum += churn( map { $row->{$_} } qw( x y z w ) );
+                }
+                if ( $err_sum < $prev_err_sum ) {
+                    $prev_err_sum = $err_sum;
+                }
+                else {
+                    $down = 0;
+                    $C{$k} = $ck_temp;
+                }
+
+                #printf ( "$C{$k}  $err_sum\n");
             }
         }
-        if ( $err_sum < $prev_err_sum ) {
-            $prev_err_sum = $err_sum;
+        if ( $C{$k} != $pre_move ) {
+            $Offset{$k}
+                = int( log( abs( $pre_move - $C{$k} ) ) / log(10) ) + 1;
         }
         push @errs, $err_sum;
     }
@@ -98,25 +124,25 @@ sub churn_loop {
     return $prev_err_sum;
 }
 
-my $err_sum = churn_loop(1);
+my $print_sum  = 1;
+my $print_diff = 1;
+my $err_sum    = churn_loop(1000);
 printf( "err_sum=%.8f\n", $err_sum );
 printf( "%s\n",           dump( \%C ) );
 
 my $loops = 0;
-while ( $err_sum > 0.01 ) {
+while ( abs($print_diff) > 0 ) {
     my $before = $err_sum;
     $err_sum = churn_loop($err_sum);
     if ( $before < $err_sum ) {
         $err_sum = churn_loop($err_sum);
     }
-    if ( $loops++ % 100 == 0 ) {
+    if ( $loops++ % 1 == 0 ) {
+        $print_diff = $print_sum - $err_sum;
         printf( "loops = %d\n",       $loops );
         printf( "  err_sum = %.8f\n", $err_sum );
+        printf( "  err_dif = %.8f\n", $print_diff );
         printf( "%s\n",               dump( \%C ) );
-    }
-    if ( $loops++ % 100 == 0 ) {
-        for my $k ( ( 'A' .. 'H' ) ) {
-            $MV{$k} = 20 * $MV{$k};
-        }
+        $print_sum = $err_sum;
     }
 }
